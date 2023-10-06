fcbac91936f6ec7289785397bbb5127a371088d6
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-06 22:27:04 +0900

    feat : embedded redis test container로 대체

---
+++ src/main/java/online/partyrun/testmanager/redis/RedisTestConfig.java
@@ -0,0 +1,27 @@
+package online.partyrun.testmanager.redis;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+public class RedisTestConfig {
+    private static final int REDIS_PORT = 6379;
+
+    @Container
+    private static GenericContainer<?> REDIS = new GenericContainer<>(DockerImageName.parse("redis:7.0.8-alpine"))
+            .withExposedPorts(REDIS_PORT);
+
+    static {
+        REDIS.start();
+        System.setProperty("spring.data.redis.host", REDIS.getHost());
+        System.setProperty("spring.data.redis.port", String.valueOf(REDIS.getMappedPort(REDIS_PORT)));
+    }
+
+    @PreDestroy
+    void preDestroy() {
+        REDIS.stop();
+    }
+}
